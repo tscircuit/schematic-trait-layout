6070c037ae8e31b41d81e8c9815b68b970b44912
seveibar <seveibar@gmail.com>
2025-05-15 16:57:18 -0700

    init snapshot

--- samples/sample1.ts
+++ samples/sample1.ts
@@ -4,15 +4,14 @@ import { chip } from "lib/builder"
 // |        |    |
 // P     ---|    |---L
 // |     |  +----+
-// +     L
-// L
+// L     L
 
 export default () => {
   const C = chip().leftpins(2).rightpins(2)
-  C.pin(1).line(-3, 0).line(0, -1).passive("vertical").line(0, -1).label()
-  C.pin(2).line(-1, 0).line(0, -1).label()
-  C.pin(3).line(1, 0).label()
-  C.pin(4).line(1, 0).label()
+  C.pin(1).line(-8, 0).line(0, -2).passive("vertical").line(0, -2).label()
+  C.pin(2).line(-3, 0).line(0, -2).label()
+  C.pin(3).line(4, 0).label()
+  C.pin(4).line(4, 0).label()
 
   return C
 }
--- tests/sample1.test.ts
+++ tests/sample1.test.ts
@@ -4,10 +4,12 @@ import sample1 from "../samples/sample1"
 test("sample1", () => {
   expect(sample1().toString()).toMatchInlineSnapshot(`
     "L
-    B  |
-    +--+L
-      L|
-      ++L
-       |"
+    |
+    B
+    |       |
+    +----L--+---L
+         |  |
+         +--+---L
+            |"
   `)
 })
