faab00e0d64a4d637da7f778dc27887f994d572b
seveibar <seveibar@gmail.com>
2025-05-15 20:41:02 -0700

    wip

--- samples/sample2.ts
+++ samples/sample2.ts
@@ -20,14 +20,8 @@ export default () => {
   const C = chip().rightpins(7)
 
   C.pin(1).line(2, 0).line(0, 2).label()
-  C.pin(2)
-    .line(3, 0)
-    .line(0, 1)
-    .line(2, 0)
-    .passive("horizontal")
-    .line(2, 0)
-    .label()
-  C.pin(3).line(5, 0).passive("horizontal").line(2, 0).label()
+  C.pin(2).line(3, 0).line(0, 1).line(2, 0).passive().line(2, 0).label()
+  C.pin(3).line(5, 0).passive().line(2, 0).label()
   C.pin(4).line(6, 0).line(0, -4).passive().line(0, -2).label()
   C.pin(5).line(3, 0).line(0, -3).passive().line(0, -2).label()
   C.pin(6).line(1, 0).line(0, -4).label() // Default label "L"
