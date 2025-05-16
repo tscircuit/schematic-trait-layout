d1d02757e85025b8a20b1863b0dc7db231690bde
seveibar <seveibar@gmail.com>
2025-05-16 10:45:26 -0700

    introduce bifurcate

--- tests/bifurcate1.test.ts
+++ tests/bifurcate1.test.ts
@@ -5,22 +5,24 @@ test("bifurcate1", () => {
   const C = circuit()
   const U1 = C.chip().leftpins(3).rightpins(3)
 
-  U1.pin(4).line(4, 0).mark("m1").line(0, 2).label()
+  U1.pin(6).line(4, 0).mark("m1").line(0, 2).label()
   U1.fromMark("m1").line(0, -2).passive().line(0, -2).label()
 
   U1.pin(5).line(2, 0).label()
-  U1.pin(6).line(1, 0).line(0, -2).label()
+  U1.pin(4).line(1, 0).line(0, -2).label()
+
+  U1.pin(1).line(-3, 0).label()
+  U1.pin(3).line(-2, 0).line(0, -2).label()
 
   expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
     "
-    ┌───┐
-    │1 6├┐  L
-    │2 5├┼L │
-    │3 4├L──┤
-    └───┘   │
-            P
-            │
-            L
+               L
+       ┌───┐   │
+    L──┤1 6├───┤
+       │2 5├─L │
+     ┌─┤3 4├┐  P
+     │ └───┘│  │
+     L      L  L
     "
   `)
 })
