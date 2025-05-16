e94127db7c61470f7f0d63e5a62f831b517c6e17
seveibar <seveibar@gmail.com>
2025-05-16 16:34:22 -0700

    bootstrap netbuilder with placeholders

---
+++ lib/builder/NetlistBuilder.ts
@@ -0,0 +1,19 @@
+import type { InputNetlist, PortReference } from "lib/input-types"
+
+export class NetlistBuilder {
+  addBox(box: InputBox): void {
+    // TODO
+  }
+
+  addNet(net: InputNet): void {
+    // TODO
+  }
+
+  connect(port1: PortReference, port2: PortReference): void {
+    // TODO
+  }
+
+  getNetlist(): InputNetlist {
+    // TODO
+  }
+}
