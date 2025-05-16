99e4589fbee2c47d2c4428e6c96a506f33b626b1
seveibar <seveibar@gmail.com>
2025-05-16 09:18:37 -0700

    minor snapshot fix

--- tests/flipX.test.ts
+++ tests/flipX.test.ts
@@ -4,7 +4,7 @@ import { circuit } from "lib/builder"
 test("flipX should flip the circuit horizontally", () => {
   const c = circuit()
   const u1 = c.chip().rightpins(2)
-  u1.pin(2).line(2, 0).label("A")
+  u1.pin(2).line(4, 0).label("A")
   u1.pin(1).line(2, 0).label("B")
 
   expect(`\n${c.toString()}\n`).toMatchInlineSnapshot(`
