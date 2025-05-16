cf1e29c95543c34e9c286b703eb4a5e30c9f1073
seveibar <seveibar@gmail.com>
2025-05-16 00:56:42 -0700

    wip bifurcate test

---
+++ tests/bifurcate1.test.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "bun:test"
+import { circuit } from "lib/builder"
+
+test("bifurcate1", () => {
+  const C = circuit()
+  const U1 = C.chip().leftpins(3).rightpins(3)
+
+  U1.pin(4).line(4, 0).mark("m1").line(0, 2).label()
+  U1.fromMark("m1").line(0, -2).passive().line(0, -2).label()
+
+  U1.pin(5).line(2, 0).label()
+  U1.pin(6).line(1, 0).line(0, -2).label()
+
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+            L
+    ┌───┐   │
+    │1 4├───┤
+    │2 5├─L │
+    │3 6├┐  P
+    └───┘│  │
+         L  L
+    "
+  `)
+})
