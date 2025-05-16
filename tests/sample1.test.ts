02065a00dfe816ae4e74a8ba09819188de6a08ae
seveibar <seveibar@gmail.com>
2025-05-15 17:05:23 -0700

    init asciigen working

--- tests/sample1.test.ts
+++ tests/sample1.test.ts
@@ -5,12 +5,14 @@ test("sample1", () => {
   const C = sample1()
   expect(C.bodyWidth).toMatchInlineSnapshot(`5`)
   expect(C.bodyHeight).toMatchInlineSnapshot(`5`)
-  expect(C.toString()).toMatchInlineSnapshot(`
-    "        -----
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+            -----
     +-------+   +---L
     |       |   |
     B    +--+   +---L
     |    |  -----
-    L    L"
+    L    L
+    "
   `)
 })
