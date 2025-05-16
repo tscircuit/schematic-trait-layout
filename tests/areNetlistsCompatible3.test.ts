f1a5c5d75a3573e13d4f7886c43e3b6cecbb8a1a
seveibar <seveibar@gmail.com>
2025-05-16 00:25:47 -0700

    wip demonstrate data for repro where netlist should be flipped

---
+++ tests/areNetlistsCompatible3.test.ts
@@ -0,0 +1,210 @@
+import { test, expect } from "bun:test"
+import template4 from "../templates/template4"
+import { circuit } from "lib/builder"
+import { normalizeNetlist } from "lib/scoring/normalizeNetlist"
+import { areNetlistsCompatible } from "lib/scoring/areNetlistsCompatible"
+
+test("areNetlistsCompatible with template4", () => {
+  // Create input circuit
+  const inputCircuit = circuit()
+  const u1 = inputCircuit.chip().rightpins(3) // chip0, global pin 1 is right pin 1
+  u1.pin(1).line(5, 0).mark("m1").line(0, -2).passive().line(0, -2).label() // Connects chip0.pin(1) to net "SignalOut"
+  u1.fromMark("m1").line(3, 0).label()
+  u1.pin(2).line(2, 0).label()
+
+  // Get template circuit
+  const templateCircuit = template4()
+
+  // Get netlists
+  const inputNetlist = inputCircuit.getNetlist()
+  const templateNetlist = templateCircuit.getNetlist()
+
+  // Normalize netlists
+  const { normalizedNetlist: normInput } = normalizeNetlist(inputNetlist)
+  const { normalizedNetlist: normTemplate } = normalizeNetlist(templateNetlist)
+
+  // Check compatibility
+  const isCompatible = areNetlistsCompatible(inputNetlist, templateNetlist)
+
+  // Create snapshots
+  expect(`\n${inputCircuit.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    │  1├────┬──L
+    │  2├─L  │
+    │  3│    P
+    └───┘    │
+             L
+    "
+  `)
+
+  expect(`\n${templateCircuit.toString()}\n`).toMatchInlineSnapshot(`
+    "
+            L
+    ┌───┐   │
+    │  1├───┤
+    │  2├─L │
+    │  3├┐  P
+    └───┘│  │
+         L  L
+    "
+  `)
+
+  expect(normInput).toMatchInlineSnapshot(`
+    {
+      "boxes": [
+        {
+          "bottomPinCount": 0,
+          "boxIndex": 0,
+          "leftPinCount": 0,
+          "rightPinCount": 3,
+          "topPinCount": 0,
+        },
+        {
+          "bottomPinCount": 1,
+          "boxIndex": 1,
+          "leftPinCount": 0,
+          "rightPinCount": 0,
+          "topPinCount": 1,
+        },
+      ],
+      "connections": [
+        {
+          "connectedPorts": [
+            {
+              "boxIndex": 0,
+              "pinNumber": 1,
+            },
+            {
+              "boxIndex": 1,
+              "pinNumber": 1,
+            },
+            {
+              "netIndex": 0,
+            },
+          ],
+        },
+        {
+          "connectedPorts": [
+            {
+              "boxIndex": 0,
+              "pinNumber": 2,
+            },
+            {
+              "netIndex": 1,
+            },
+          ],
+        },
+        {
+          "connectedPorts": [
+            {
+              "boxIndex": 1,
+              "pinNumber": 2,
+            },
+            {
+              "netIndex": 2,
+            },
+          ],
+        },
+      ],
+      "nets": [
+        {
+          "netIndex": 0,
+        },
+        {
+          "netIndex": 1,
+        },
+        {
+          "netIndex": 2,
+        },
+      ],
+    }
+  `)
+
+  expect(normTemplate).toMatchInlineSnapshot(`
+    {
+      "boxes": [
+        {
+          "bottomPinCount": 0,
+          "boxIndex": 0,
+          "leftPinCount": 0,
+          "rightPinCount": 3,
+          "topPinCount": 0,
+        },
+        {
+          "bottomPinCount": 1,
+          "boxIndex": 1,
+          "leftPinCount": 0,
+          "rightPinCount": 0,
+          "topPinCount": 1,
+        },
+      ],
+      "connections": [
+        {
+          "connectedPorts": [
+            {
+              "boxIndex": 0,
+              "pinNumber": 1,
+            },
+            {
+              "boxIndex": 1,
+              "pinNumber": 1,
+            },
+            {
+              "netIndex": 0,
+            },
+          ],
+        },
+        {
+          "connectedPorts": [
+            {
+              "boxIndex": 0,
+              "pinNumber": 2,
+            },
+            {
+              "netIndex": 1,
+            },
+          ],
+        },
+        {
+          "connectedPorts": [
+            {
+              "boxIndex": 0,
+              "pinNumber": 3,
+            },
+            {
+              "netIndex": 2,
+            },
+          ],
+        },
+        {
+          "connectedPorts": [
+            {
+              "boxIndex": 1,
+              "pinNumber": 2,
+            },
+            {
+              "netIndex": 3,
+            },
+          ],
+        },
+      ],
+      "nets": [
+        {
+          "netIndex": 0,
+        },
+        {
+          "netIndex": 1,
+        },
+        {
+          "netIndex": 2,
+        },
+        {
+          "netIndex": 3,
+        },
+      ],
+    }
+  `)
+
+  expect(isCompatible).toBe(true)
+})
