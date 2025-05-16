2f6bee6e339bbb613065c4663fb6ce84586fa40a
seveibar <seveibar@gmail.com>
2025-05-15 17:30:05 -0700

    add script todos

---
+++ scripts/generate-sample-comments.ts
@@ -0,0 +1,8 @@
+#! /usr/bin/env bun
+
+// TODO get all the files in the samples directory
+
+// TODO for each file, import the sample and stringify it
+
+// TODO write the stringified sample to the top of the file in a comment
+// removing any existing docstring-style comment
