b3d075bf39ce9136c65cf5983247766f74498f6e
seveibar <seveibar@gmail.com>
2025-05-16 01:24:41 -0700

    repro flipping circuit does not fix pin numbering

---
+++ tests/flipX.test.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "bun:test"
+import { circuit } from "lib/builder"
+
+test("flipX should flip the circuit horizontally", () => {
+  const c = circuit()
+  const u1 = c.chip().rightpins(2)
+  u1.pin(2).line(2, 0).label("A")
+  u1.pin(1).line(2, 0).label("B")
+
+  expect(`\n${c.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    │  2├─A
+    │  1├─B
+    └───┘
+    "
+  `)
+
+  c.flipX()
+
+  expect(`\n${c.toString()}\n`).toMatchInlineSnapshot(`
+    "
+      ┌───┐
+    A─┤2  │
+    B─┤1  │
+      └───┘
+    "
+  `)
+})
