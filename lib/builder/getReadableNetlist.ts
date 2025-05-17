7eeb0ada295720f352abb7fd075ea94f6c95b6ab
seveibar <seveibar@gmail.com>
2025-05-16 19:03:41 -0700

    wip getReadableNetlist

---
+++ lib/builder/getReadableNetlist.ts
@@ -0,0 +1,10 @@
+import type { InputNetlist } from "lib/input-types"
+
+/**
+ * Returns a string representation of the netlist that is easy to read.
+ *
+ * This is useful for debugging.
+ */
+export const getReadableNetlist = (netlist: InputNetlist): string => {
+  // TODO
+}
