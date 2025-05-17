b880202201b14a68504c84608d2918caf8762720
seveibar <seveibar@gmail.com>
2025-05-16 22:02:57 -0700

    fix repro

--- tests/mergeCircuits2.test.ts
+++ tests/mergeCircuits2.test.ts
@@ -17,29 +17,32 @@ test("mergeCircuits2", () => {
     "
   `)
 
-  const T1_flipped = T1.clone().flipX()
+  const T2 = circuit()
+  const T2_U1 = T2.chip().rightpins(2)
+  T2_U1.pin(1).line(2, 0).label()
+  T2_U1.pin(2).line(2, 0).label()
 
-  expect(`\n${T1_flipped.toString()}\n`).toMatchInlineSnapshot(`
+  expect(`\n${T2.toString()}\n`).toMatchInlineSnapshot(`
     "
     ┌───┐
-    │  1├─L
     │  2├─L
+    │  1├─L
     └───┘
     "
   `)
 
   const merged = mergeCircuits({
     circuit1: T1,
-    circuit2: T1_flipped,
+    circuit2: T2,
     circuit1ChipId: T1_U1.chipId,
-    circuit2ChipId: T1_U1.chipId,
+    circuit2ChipId: T2_U1.chipId,
   })
 
   expect(`\n${merged.toString()}\n`).toMatchInlineSnapshot(`
     "
       ┌───┐
-    L─┤1 4├─L
-    L─┤2 3├─L
+    L─┼───┼─L
+    L─┤   ├─L
       └───┘
     "
   `)
