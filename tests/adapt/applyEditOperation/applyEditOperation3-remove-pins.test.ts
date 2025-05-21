a43f59caf663ff0339ae2f058b189e5ad377d6a0
seveibar <seveibar@gmail.com>
2025-05-21 15:36:31 -0700

    confirm pin removal working

--- tests/adapt/applyEditOperation/applyEditOperation3-remove-pins.test.ts
+++ tests/adapt/applyEditOperation/applyEditOperation3-remove-pins.test.ts
@@ -4,6 +4,17 @@ import { applyEditOperation } from "lib/adapt/applyEditOperation"
 test("remove pins from side", () => {
   const C = new CircuitBuilder()
   const U = C.chip().leftpins(3).rightpins(1)
+
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    ┤1  │
+    ┤2  │
+    ┤3 4├
+    └───┘
+    "
+  `)
+
   applyEditOperation(C, {
     type: "remove_pins_from_side",
     chipId: U.chipId,
@@ -13,4 +24,11 @@ test("remove pins from side", () => {
   })
   expect(U.leftPinCount).toBe(1)
   expect(U.totalPinCount).toBe(2)
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    ┤1 2├
+    └───┘
+    "
+  `)
 })
