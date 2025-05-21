104788d402bd4a54c5c61d6403a2de7097c5b742
seveibar <seveibar@gmail.com>
2025-05-21 16:48:45 -0700

    init compute edit operations to fix pin subset netlist

--- tests/adapt/computeEditOperationsToFixPinSubsetNetlist/computeEditOperationsToFixPinSubsetNetlist1.test.ts
+++ tests/adapt/computeEditOperationsToFixPinSubsetNetlist/computeEditOperationsToFixPinSubsetNetlist1.test.ts
@@ -1,11 +1,39 @@
 import { test, expect } from "bun:test"
+import { computeEditOperationsToFixPinSubsetNetlist } from "lib/adapt/computeEditOperationsToFixPinSubsetNetlist"
 import { CircuitBuilder } from "lib/builder"
 
 test("computeEditOperationsToFixPinSubsetNetlist1", () => {
-  const C = new CircuitBuilder()
-  const U = C.chip().leftpins(2).rightpins(2)
+  const target = new CircuitBuilder()
+  const U = target.chip().leftpins(2).rightpins(2)
   U.pin(1).line(-1, 0).label()
-  U.pin(2).line(-2, 0).line(0, -1).label()
 
-  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot()
+  expect(`\n${target.toString()}\n`).toMatchInlineSnapshot(`
+    "
+     ┌───┐
+    L┤1 4├
+     ┤2 3├
+     └───┘
+    "
+  `)
+
+  const template = new CircuitBuilder()
+  const V = template.chip().leftpins(2).rightpins(2)
+
+  expect(`\n${template.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    ┤1 4├
+    ┤2 3├
+    └───┘
+    "
+  `)
+
+  const operations = computeEditOperationsToFixPinSubsetNetlist({
+    chipId: template.chips[0]!.chipId,
+    currentNetlist: template.getNetlist(),
+    pinNumber: 1,
+    targetNetlist: target.getNetlist(),
+  })
+
+  expect(operations).toMatchInlineSnapshot()
 })
