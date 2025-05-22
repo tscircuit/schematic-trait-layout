20e7cdb0ed343070277909f2b85de9e37889da2b
seveibar <seveibar@gmail.com>
2025-05-21 17:36:52 -0700

    add multi required operations to test

--- tests/adapt/adaptTemplateToTarget/adaptTemplateToTarget1.test.ts
+++ tests/adapt/adaptTemplateToTarget/adaptTemplateToTarget1.test.ts
@@ -5,11 +5,28 @@ import { adaptTemplateToTarget } from "lib/adapt/adaptTemplateToTarget"
 test("adaptTemplateToTarget adds missing pins on a side", () => {
   /* target circuit --------------------------------------------------- */
   const target = new CircuitBuilder()
-  target.chip().leftpins(2).rightpins(1) // 2 left pins required
+  target.chip().leftpins(2).rightpins(2)
 
   /* template circuit (one pin short) --------------------------------- */
   const template = new CircuitBuilder()
   template.chip().leftpins(1).rightpins(1)
+
+  expect(`\n${target.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    ┤1 4├
+    ┤2 3├
+    └───┘
+    "
+  `)
+
+  expect(`\n${template.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    ┤1 2├
+    └───┘
+    "
+  `)
 
   /* run adaptation --------------------------------------------------- */
   const { appliedOperations } = adaptTemplateToTarget({
@@ -26,6 +43,15 @@ test("adaptTemplateToTarget adds missing pins on a side", () => {
         ],
         "chipId": "chip0",
         "side": "left",
+        "type": "add_pin_to_side",
+      },
+      {
+        "betweenPinNumbers": [
+          3,
+          4,
+        ],
+        "chipId": "chip0",
+        "side": "right",
         "type": "add_pin_to_side",
       },
     ]
