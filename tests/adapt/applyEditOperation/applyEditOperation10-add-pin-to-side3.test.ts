484c07bef6c6d9154fbca7493d9b295bc9f9572d
seveibar <seveibar@gmail.com>
2025-05-21 17:39:23 -0700

    add test confirming add pin on right side works

---
+++ tests/adapt/applyEditOperation/applyEditOperation10-add-pin-to-side3.test.ts
@@ -0,0 +1,40 @@
+import { test, expect } from "bun:test"
+import { CircuitBuilder } from "lib/builder"
+import { applyEditOperation } from "lib/adapt/applyEditOperation"
+
+test("add pin to side", () => {
+  const C = new CircuitBuilder()
+  const U = C.chip().leftpins(1).rightpins(2)
+  U.pin(2).line(2, 0).label()
+  U.pin(3).line(2, 0).line(0, 1).label()
+
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐ L
+    │  3├─┘
+    ┤1 2├─L
+    └───┘
+    "
+  `)
+
+  applyEditOperation(C, {
+    type: "add_pin_to_side",
+    chipId: U.chipId,
+    side: "right",
+    betweenPinNumbers: [3, 4],
+  })
+
+  // Expect pin 1, then new pin, then pin 2 (now pin 3)
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+    ┌───┐
+    │  4├ L
+    │  3├─┘
+    ┤1 2├─L
+    └───┘
+    "
+  `)
+
+  expect(U.leftPinCount).toBe(1)
+  expect(U.totalPinCount).toBe(4)
+})
