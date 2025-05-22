b09f76f4d14cafe8469dd98406966fcb2fef77bc
seveibar <seveibar@gmail.com>
2025-05-21 17:02:43 -0700

    snapshot for passive add

--- tests/adapt/applyEditOperation/applyEditOperation4-add-passive.test.ts
+++ tests/adapt/applyEditOperation/applyEditOperation4-add-passive.test.ts
@@ -5,10 +5,24 @@ test("add passive to pin", () => {
   const C = new CircuitBuilder()
   const U = C.chip().leftpins(1)
   U.pin(1).line(-1, 0)
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+     ┌───┐
+    ─┤1  │
+     └───┘
+    "
+  `)
   applyEditOperation(C, {
     type: "add_passive_to_pin",
     chipId: U.chipId,
     pinNumber: 1,
   })
   expect(C.chips.length).toBe(2) // original + passive
+  expect(`\n${C.toString()}\n`).toMatchInlineSnapshot(`
+    "
+     ┌───┐
+    P┤1  │
+     └───┘
+    "
+  `)
 })
