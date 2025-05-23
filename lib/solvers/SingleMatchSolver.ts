362b8cda6715bcd166e1ffef65717764d2cd6a1a
seveibar <seveibar@gmail.com>
2025-05-23 12:28:28 -0700

    wip todo for single match solver

--- lib/solvers/SingleMatchSolver.ts
+++ lib/solvers/SingleMatchSolver.ts
@@ -1,5 +1,6 @@
 import type { InputNetlist } from "lib/input-types"
 import { BaseSolver } from "./BaseSolver"
+import type { CircuitBuilder } from "lib/builder"
 
 /**
  * Find the best match template for a netlist
@@ -7,10 +8,17 @@ import { BaseSolver } from "./BaseSolver"
 export class SingleMatchSolver extends BaseSolver {
   inputNetlist: InputNetlist
 
+  outputBestMatchTemplate: CircuitBuilder | null = null
+
   constructor(opts: {
     inputNetlist: InputNetlist
   }) {
     super()
     this.inputNetlist = opts.inputNetlist
+  }
+
+  _step() {
+    // TODO the single matcher should take the inputNetlist and compare it to
+    // the templates (see templates/index.ts) then set the outputBestMatchTemplate
   }
 }
